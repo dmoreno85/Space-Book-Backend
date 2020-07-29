acfdf24b10db39a20f7735e9fd4a1127c3925f58
David <dmoreno788@gmail.com>
2020-07-29 15:45:52 +0200

    Congifurado mongoose.js

---
+++ config/mongoose.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const mongo_URI = 'mongodb://localhost:27017/space-book';
+mongoose.connect(mongo_URI,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useCreateIndex:true,
+    useFindAndModify:false
+})
+.then(()=>console.log('Conectado satisfactoriamente a MongoDB'))
+.catch(console.error)
