52da959f2141d074149e02b739fb4aa177b7124f
David <dmoreno788@gmail.com>
2020-07-29 17:25:23 +0200

    Añadido coments en UserSchema, añadido UserSchema.metods.toJSON

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const ObjectId = mongoose.Schema.Types.ObjectId;
 const UserSchema = new mongoose.Schema({
     name: String,
     email: {
@@ -12,6 +12,21 @@ const UserSchema = new mongoose.Schema({
         required: true,
     },
     tokens: [],
+    coments: [{
+        text: String,
+        UserId: {
+            type: ObjectId,
+            ref: 'User'
+        }
+    }],
+    timestamps: true,
 });
 
+UserSchema.methods.toJSON= function (params){
+    const user = this._doc;
+    delete user.tokens;
+    delete user.password;
+    delete user.__v;
+    return user;
+}
 module.exports = mongoose.model('User', UserSchema);
